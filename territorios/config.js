31ac2a058ea4be43a4fe02cb0f7e04770d6ad575
abelbour <abelbour@users.noreply.github.com>
2024-11-08 14:18:46 -0300

    Create config.js

---
+++ territorios/config.js
@@ -0,0 +1,4 @@
+// config.js
+const googleSheetId = '1g6ZP3QrlF95YaaN21-wCeXY2h05D7VK7nYAXvOehdtA'
+const territoriosURL = 'https://sheets.googleapis.com/v4/spreadsheets/' + googleSheetId + '/values/TerritoriosJson?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc';
+const gruposURL = 'https://sheets.googleapis.com/v4/spreadsheets/' + googleSheetId + '/values/Grupos?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc';
